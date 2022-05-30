743fad35fe8170031ded7aefd6c1d0a4c133716d
Michael W <walkerrepair255@yahoo.com>
2022-05-29 22:19:25 -0400

    More Practise

--- Exercise/Method.cs
+++ Exercise/Method.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-namespace Exercise
+﻿namespace Exercise
 {
     internal class Method
     {
@@ -37,6 +35,57 @@ namespace Exercise
                 return newIf;
             }
             return ifCheck;
+        }
+
+        public static string FirstAndLastSwap(string str)
+        {
+            return str.Length > 1 ? str[^1..] + str[1..^1] + str[..1] : str;
+        }
+
+        public static string FourTimesFirstTwo(string str)
+        {
+            return str.Length < 2 ? str : SubStringMultiFour(str);
+        }
+        public static string SubStringMultiFour(string strLoop)
+        {
+            string result = "";
+            for (int i = 0; i < 4; i++)
+            {
+                result += strLoop[..2];
+            }
+            return result;
+        }
+        public static bool ThreeOrSeven(int x)
+        {
+            return x % 3 == 0 || x % 7 == 0;
+        }
+        public static void ReverseInput()
+        {
+            var x = Console.ReadLine();
+            var y = Console.ReadLine();
+            var z = Console.ReadLine();
+            Console.WriteLine(z + y + x);
+        }
+        public static void NumberPyramid()
+        {
+            Console.Write("Input a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the desired width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            int height = width;
+            for (int row = 0; row < height; row++)
+            {
+
+                for (int column = 0; column < width; column++)
+                {
+                    Console.Write(num);
+                }
+                Console.WriteLine("\n");
+                width -= 1;
+
+            }
         }
 
     }
